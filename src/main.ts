dcba23f6dc2046c31c4d88516fb747d5871152f5
octavesop <octavesop@gmail.com>
2023-12-04 03:09:47 +0900

    :sparkles: Class-validator 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import { Logger } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
@@ -11,9 +15,24 @@ const bootstrap = async () => {
 
   app.setGlobalPrefix('api');
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // dto에서 class-validator가 붙어있지 않은 속성은 모두 제거됨.
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 값이 들어오면 무조건 400을 반환함.
+      forbidUnknownValues: true, // 확인되지 않은 값이 들어오면 무조건 400을 반환함.
+      transform: true, // 클라이언트에서 받은 값을 백엔드에서 지정한 값으로 형변환을 거침.
+      transformOptions: {
+        enableImplicitConversion: true, // class-transformer가 ts 유형을 기준으로 형변환을 진행.
+      },
+      disableErrorMessages:
+        process.env.NODE_ENV === 'development' ? false : true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('Nest.js boilorplate')
-    .setDescription('Nest.js boilorplate')
+    .setTitle('줌줌투어 코딩테스트')
+    .setDescription('줌줌투어 코딩테스트')
     .setVersion('0.0.1')
     .build();
 
