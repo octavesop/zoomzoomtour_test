d1ed55885a97cfda9cc8435b9aa2530857a8f239
octavesop <octavesop@gmail.com>
2023-12-04 03:02:58 +0900

    :sparkles: Config 및 DB 연결 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.development',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('MYSQL_HOST'),
+        port: configService.get('MYSQL_PORT'),
+        username: configService.get('MYSQL_USERNAME'),
+        password: configService.get('MYSQL_PASSWORD'),
+        database: configService.get('MYSQL_DATABASE'),
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+        synchronize:
+          configService.get('NODE_ENV') === 'development' ? true : false,
+        logging: true,
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+  const configService = app.get(ConfigService);
+  const logger = new Logger('bootstrap');
+
+  app.setGlobalPrefix('api');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nest.js boilorplate')
+    .setDescription('Nest.js boilorplate')
+    .setVersion('0.0.1')
+    .build();
+
+  const swaggerURI = '/docs';
+
+  SwaggerModule.setup(
+    swaggerURI,
+    app,
+    SwaggerModule.createDocument(app, swaggerConfig),
+  );
+
+  await app.listen(
+    configService.get('PORT'),
+    configService.get('HOST'),
+    async () => {
+      logger.log(
+        `Application is successfully running on ${await app.getUrl()}`,
+      );
+      logger.log(
+        `Application Environment is **${configService.get('NODE_ENV')}** Mode`,
+      );
+
+      if (configService.get('NODE_ENV') !== 'production') {
+        logger.verbose(`You can check ${await app.getUrl()}${swaggerURI}`);
+      }
+    },
+  );
+};
 bootstrap();
